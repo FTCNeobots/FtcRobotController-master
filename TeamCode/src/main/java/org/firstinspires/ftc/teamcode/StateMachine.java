980c85fdfdcb9bbf0c52e7cec4c5d6b0b139e8eb
Jaap2008 <beemsterjaap1@gmail.com>
2025-05-24 14:43:46 +0200

    Should have config working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateMachine.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.ConfigSubSonic.ConfigAuto;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -33,36 +35,7 @@ public class StateMachine extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        leftBackDrive = hardwareMap.dcMotor.get("LBD");
-        leftFrontDrive = hardwareMap.dcMotor.get("LFD");
-        rightBackDrive = hardwareMap.dcMotor.get("RBD");
-        rightFrontDrive = hardwareMap.dcMotor.get("RFD");
-
-        liftMotor = hardwareMap.dcMotor.get("lift");
-        swingMotor = hardwareMap.dcMotor.get("swing");
-        clawServo = hardwareMap.get(Servo.class, "claw");
-
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        //dit hoort niet te hoeven, maar zo werkt het?
-        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        swingMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        swingMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
+        ConfigAuto(leftBackDrive, leftFrontDrive, rightBackDrive, rightFrontDrive, liftMotor, swingMotor, clawServo);
 
 
         waitForStart();
